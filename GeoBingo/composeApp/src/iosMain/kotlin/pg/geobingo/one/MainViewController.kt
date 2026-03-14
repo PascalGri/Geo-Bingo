88301af2c667126dd5fa3b3bfec928c2221461d9
Pascalgri <pascal.grimm@outlook.de>
2026-03-14 13:54:50 +0100

    Add initial iOS app structure and integrate Compose
    - Created initial project structure for GeoBingo iOS app.
    - Added ContentView with integration for Compose using UIViewControllerRepresentable.
    - Configured Info.plist with necessary settings.
    - Added asset catalogs for main and preview assets.
    - Established main entry point for the app in iOSApp.swift.
    - Updated settings.gradle.kts for project configuration and dependencies.

---
+++ GeoBingo/composeApp/src/iosMain/kotlin/pg/geobingo/one/MainViewController.kt
@@ -0,0 +1,5 @@
+package pg.geobingo.one
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ GeoBingo/composeApp/src/jvmMain/kotlin/pg/geobingo/one/main.kt
@@ -0,0 +1,13 @@
+package pg.geobingo.one
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "GeoBingo",
+    ) {
+        App()
+    }
+}
